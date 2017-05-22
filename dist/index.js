8fa84a25716d1ae0ebac0cc917bfc2083660cdf6
Lego Chiang <qas612820704@gmail.com>
2017-05-22 21:48:17 +0800

    Build to dist, version 2.0.0

---
+++ dist/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.TimelineEvent = exports.Timeline = undefined;
+
+var _Timeline = require('./Timeline');
+
+var _Timeline2 = _interopRequireDefault(_Timeline);
+
+var _TimelineEvent = require('./TimelineEvent');
+
+var _TimelineEvent2 = _interopRequireDefault(_TimelineEvent);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.Timeline = _Timeline2.default;
+exports.TimelineEvent = _TimelineEvent2.default;
